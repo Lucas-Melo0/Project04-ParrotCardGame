2832ac501289e1cd24dbc0de120361a88cafa1ef
LucasM <lucassss8v@gmail.com>
2022-06-15 13:13:24 -0300

    Implemented shuffling of the cards

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Global variables 
 
 let numberCards = 0;
 let cardsInGame = [];
 let cardHtml;
+
+// Functions 
+
 howMany();
 function howMany(){
 do { numberCards = prompt("Com quantas cartas deseja jogar?") 
@@ -11,22 +15,30 @@ insertCards()
 function insertCards(){
     cardsInGame = [];
     cardHtml = document.querySelector(".card_set01");
-    for (let i =0; i < numberCards; i++) {
-        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'><img src='images/front 5.png'></div>");
-
+    for (let i =0; i < numberCards/2; i++) {
+        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'><img src='images/front 5.png'></div>"); 
+        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'></div>");       
     }
-    cardHtml.innerHTML = cardsInGame;
+    cardsInGame = cardsInGame.sort(compare)
+    cardHtml.innerHTML = cardsInGame.join("");
+
 
     cardsInGame = [];
     cardHtml = document.querySelector(".card_set02");
-    for (let i =0; i < numberCards; i++) {
-        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'><img src='images/front 5.png'></div>");
+    for (let i =0; i < numberCards/2; i++) {
+        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'><img src='images/front 5.png'></div>")
+        cardsInGame.push(cardHtml.innerHTML = "<div class='card_back'></div>");
+        
+        
 
     }
-    cardHtml.innerHTML = cardsInGame;
-    
+    cardsInGame = cardsInGame.sort(compare)
+    cardHtml.innerHTML = cardsInGame.join("");
 }
 
+function compare(){
+    return Math.random() - 0.5;
+}
 
     
        
